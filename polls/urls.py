d4854d363949a44d3614711dd98fe7dee59eb2a9
Khashayar <khashayargatmiry@gmail.com>
2017-07-29 19:33:16 +0430

    mainfirst

--- polls/urls.py
+++ polls/urls.py
@@ -3,9 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<id>[0-9]+)/$' , views.mainpage),
+    url(r'^(?P<id>[0-9]+)/$' , views.mainpage , name='index'),
     url(r'^(?P<id>[0-9]+)/financial/(?P<type>[\w\-]+)/$' , views.financial , name='financial'),
     url(r'^(?P<id>[0-9]+)/reservation/(?P<reserve>[0-9]+)/$', views.reservation, name='reservation'),
+    url(r'^(?P<id>[0-9]+)/neighbors/$', views.neighbors, name='neighbors'),
+    url(r'^(?P<id>[0-9]+)/guest/(?P<guest_id>[\w\-]+)/$', views.guest, name='guest'),
+    url(r'^(?P<id>[0-9]+)/validation/$', views.validation, name='validation'),
+    url(r'^(?P<id>[0-9]+)/owner/$', views.owner, name='owner'),
+
 
     #url(r'^$(?P<question_id>[0-9]+)/')
 ]
--- polls/views.py
+++ polls/views.py
@@ -12,10 +12,22 @@ from django.template import loader
 def mainpage(request , id):
    # ApartmentFormSet = modelformset_factory(Apartment , fields=('number' , 'floor_num'))
    # formset = ApartmentFormSet()
-    return render(request, 'polls/index.html' , {})
+    return render(request, 'polls/index.html' , {'id' : id})
 
 def financial(request , id , type):
-    return render(request, 'polls/financial.html', {})
+    return render(request, 'polls/financial.html', {'type' : type , 'id': id})
 
 def reservation(request , id , reserve):
-    return render(request, 'polls/reservation.html' , {'hasreserve' : reserve})
+    return render(request, 'polls/reservation.html' , {'hasreserve' : reserve , 'id' : id})
+
+def neighbors(request , id):
+    return render(request , 'polls/neighbors.html' , {'id': id})
+
+def guest(request , id , guest_id):
+    return render(request , 'polls/guest.html' , {'my_id' : id , 'guest_id' : guest_id})
+
+def validation(request , id):
+    return render(request , 'polls/validation.html' , {'id' : id})
+
+def owner(request , id):
+    return render(request, 'polls/owner.html', {'id': id})
