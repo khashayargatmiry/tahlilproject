e6bc41b487d8b32f14bd40a4a9d36be7722ccd9f
Ali Mohammad Faraji <alimfaraji@users.noreply.github.com>
2017-08-11 16:26:03 +0430

    python3Update
    updating to python3, a little change

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from models import Neighbor
-from models import Bank
-from models import Apartment
+
+from .models import Neighbor
+from .models import Bank
+from .models import Apartment
 # Register your models here.
 
 admin.site.register(Neighbor)
